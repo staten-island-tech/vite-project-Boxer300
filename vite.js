0509ef90fd4ebac8b705f6b67109bf9b84ad8083
Boxer300 <romano2789@nycstudents.net>
2025-11-24 12:30:37 -0500

    h

---
+++ vite.js
@@ -0,0 +1,52 @@
+    const memes = [
+  {
+    name: "Distracted Boyfriend",
+    category: "funny",
+    image: "https://i.imgflip.com/1ur9b0.jpg"
+  },
+  {
+    name: "Drake Hotline Bling",
+    category: "funny",
+    image: "https://i.imgflip.com/30b1gx.jpg"
+  },
+  {
+    name: "Woman Yelling at Cat",
+    category: "funny",
+    image: "https://i.imgflip.com/3si4.jpg"
+  },
+  {
+    name: "Surprised Pikachu",
+    category: "reaction",
+    image: "https://i.imgflip.com/2kbn1e.jpg"
+  },
+  {
+    name: "Change My Mind",
+    category: "reaction",
+    image: "https://i.imgflip.com/24y43o.jpg"
+  },
+  {
+    name: "Two Buttons",
+    category: "relatable",
+    image: "https://i.imgflip.com/1g8my4.jpg"
+  },
+  {
+    name: "Roll Safe",
+    category: "smart",
+    image: "https://i.imgflip.com/1h7in3.jpg"
+  },
+  {
+    name: "Mocking Spongebob",
+    category: "funny",
+    image: "https://i.imgflip.com/1otk96.jpg"
+  },
+  {
+    name: "Epic Handshake",
+    category: "wholesome",
+    image: "https://i.imgflip.com/28j0te.jpg"
+  },
+  {
+    name: "Dog in Burning Room (This is Fine)",
+    category: "dark humor",
+    image: "https://i.imgflip.com/wxica.jpg"
+  }
+];
