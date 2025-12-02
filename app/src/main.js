710e98288a10ed27abc4740fba28212342113393
Boxer300 <romano2789@nycstudents.net>
2025-12-02 11:50:31 -0500

    Update main.js

--- app/src/main.js
+++ app/src/main.js
@@ -1,24 +1,84 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+ const clothes = [
+  {
+    name: "Distracted Boyfriend",
+    category: "funny",
+    image: "https://media.wired.com/photos/59a459d3b345f64511c5e3d4/master/pass/MemeLoveTriangle_297886754.jpg"
+  },
+  {
+    name: "Drake yes no",
+    category: "funny",
+    image: "https://preview.redd.it/3tyfis4savq21.jpg?width=640&crop=smart&auto=webp&s=9c0b3556b61e317ad1ec6e4c6372ef08ded5055a"
+  },
+  {
+    name: "Woman Yelling at Cat",
+    category: "funny",
+    image: "https://ih1.redbubble.net/image.5753717992.6682/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+  },
+  {
+    name: "Surprised Pikachu",
+    category: "reaction",
+    image: "https://external-preview.redd.it/0UF4Gqxwe330DKxNz1_GpmkwIDX0e_TyJdXbbkDVm78.jpg?width=1080&crop=smart&auto=webp&s=4137aba5c4eea563b3c1b5c38e9a5d7ea4edb6c9"
+  },
+  {
+    name: "Change My Mind",
+    category: "reaction",
+    image: "https://preview.redd.it/change-my-mind-template-without-steven-crowder-v0-aunz7dyoumza1.jpg?width=640&crop=smart&auto=webp&s=fa1b5f7804d61f6462125cf25ae5fc915a14ee78"
+  },
+  {
+    name: "Two Buttons",
+    category: "relatable",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEB6fYN0oLYwdzjJOLN2ppKGsZLCNApyXkIg&s"
+  },
+  {
+    name: "Roll Safe",
+    category: "smart",
+    image: "https://a.pinatafarm.com/702x395/ac64fb8667/roll-safe.jpg"
+  },
+  {
+    name: "Mocking Spongebob",
+    category: "funny",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwp2Vu-wHrCop5hGwVoPRtcGfV1jQxSuSSTw&s"
+  },
+  {
+    name: "Epic Handshake",
+    category: "wholesome",
+    image: "https://a.pinatafarm.com/1787x1280/1e44f0e695/epic-handshake.jpg"
+  },
+  {
+    name: "Dog in Burning Room (This is Fine)",
+    category: "dark humor",
+    image: "https://pbs.twimg.com/media/Fsi5-9vWcAYK3xp.jpg"
+  }
+];
+ clothes.forEach((item) => inject(item))
+function inject(clothes) {
+   document.querySelector(".container").insertAdjacentHTML(
+    "afterbegin",
+     `<div class="display-card">
+      <img class="display-image" src="${clothes.image}" />
+       <h2 class="display-category">${clothes.name} </h2>
+      <h3 class="display-title">${clothes.category} </h3>
+       <button class="add text">Add text</button>
+    </div>`
+   );
+  }
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("Add text")) {
+    addToCart(event);
+  }
+}); 
+document.querySelector(".btn").addEventListener("click", function () {
+  console.log("ets")
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+  }
+});
+function filter(category){
+const filtered = clothes.filter(cloth => cloth.category === category);
+return filtered;
+}
